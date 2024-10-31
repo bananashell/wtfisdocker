a5ed4f445fbe54cb4faa812600a04358dfd57363
bananashell <joakim.jaderberg@spp.se>
2024-10-31 21:25:56 +0100

    add superhero-service using elysia

--- superhero-service/index.ts
+++ superhero-service/index.ts
@@ -1,21 +1,45 @@
-import Fastify from "fastify";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { superHeroesTable } from "./src/db/schema";
+import { Elysia, t, error } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const fastify = Fastify({ logger: true });
+const db = drizzle(process.env.DATABASE_URL!);
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
 
-// Declare a route
-fastify.get("/superheroes", async (request, reply) => {
-    return { superheroes: ["Superman", "Batman", "Wonder Woman"] };
-});
+const app = new Elysia()
+    .use(
+        swagger({
+            documentation: {
+                info: { title: "Superhero Service", version: "1.0" },
+            },
+        })
+    )
+    .get("/", () => "Hello Elysia")
+    .get("/superheroes", async () => {
+        const allSuperHeroes = await db.select().from(superHeroesTable);
+        return { superheroes: allSuperHeroes };
+    })
+    .post(
+        "/superheroes",
+        async ({ body: { name } }) => {
+            if (!name) {
+                return error("Bad Request", "Name is required");
+            }
 
-// Run the server!
-const start = async () => {
-    try {
-        await fastify.listen({ port: 3000 });
-        console.log(`Server is running at http://localhost:3000`);
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-};
+            try {
+                const a = await db.insert(superHeroesTable).values({ name });
 
-start();
+                return {
+                    message: `${a.rowCount} superheroes added successfully`,
+                };
+            } catch (err) {
+                return error("Internal Server Error", err);
+            }
+        },
+        {
+            body: t.Object({ name: t.String() }),
+        }
+    )
+    .listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+    });
